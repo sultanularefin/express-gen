eec2a23fb711eba18f8953ec464178f2b347eac2
sultanularefin <mhmdarefin@gmail.com>
2021-09-18 08:57:32 +0600

    node_nodules git ignore added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,125 @@
 var express = require('express');
-var router = express.Router();
+// var router = express.Router();
+
+// const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const app = express();
+
+
 
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
 
-module.exports = router;
+
+
+
+
+app.use(
+    fileUpload()
+);
+
+/*
+router.get('/', function(
+    req,
+    res,
+    next) {
+
+  // res.render('index', { title: 'Express' });
+
+  res.sendFile(path.join(__dirname, "index.html" ))
+
+
+});
+*/
+
+/*
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
+   // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3001/","http://localhost:3001");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+
+app.get("/", (
+    req,
+    res) => {
+    res.sendFile(path.join(__dirname, "/../index.html"));
+});
+
+// + '/../../foo.bar'
+app.post(
+    "/upload",
+    (
+        req,
+        res
+
+    ) => {
+
+        if (!req.files) {
+
+            return res.status(400).send("No files were uploaded.");
+        }
+
+
+        const file = req.files.myFile;
+        // const path = __dirname + "/files/" + file.name;
+
+        // return res.status(200).send(path);
+
+        // return res.status(400).send(file.name);
+
+
+        // 400 bad request.
+        // 200 ok
+        // 201 created.
+        // 202 accepted.
+
+
+
+        /*
+        const path = __dirname + "../files/" + file.name;
+
+        file.mv(path, (err) => {
+          if (err) {
+            return res.status(500).send(err);
+          }
+          return res.send({ status: "success", path: path });
+        });
+
+        */
+
+
+        const path = __dirname + "/files/" + file.name;
+
+        // return res.status(200).send(path);
+
+        // 500 Internal Server Error
+
+        file.mv(path, (err) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            return res.send({ status: "success", path: path });
+        });
+    });
+
+// module.exports = router;
+
+module.exports = app;
